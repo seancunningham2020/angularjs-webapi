85a71a05f4214f4f31423c50f9c204b22dda3b83
seancunningham2020 <s.cunningham@livebookings.london>
2016-06-22 16:27:29 +0100

    Refactoring
    Code improvements, consistent linking of controllers to views.

--- angularjs-webapi/angularjs-webapi.web/App/Controllers/mainController.js
+++ angularjs-webapi/angularjs-webapi.web/App/Controllers/mainController.js
@@ -1,4 +1,6 @@
-﻿app.controller('mainController', [
+﻿"use strict";
+
+app.controller('mainController', [
     '$scope', function($scope) {
         $scope.message = "Welcome to this project, which uses an AngularJS single page application to make calls to a Web API back-end.";
     }
--- angularjs-webapi/angularjs-webapi.web/App/app.js
+++ angularjs-webapi/angularjs-webapi.web/App/app.js
@@ -3,22 +3,19 @@
 var app = angular.module('myApp', ['ngRoute']);
 
 app.config([
-    '$routeProvider', function($routeProvider) {
+    '$routeProvider', '$locationProvider', function($routeProvider) {
         $routeProvider
             .when('/', {
-                controller: 'mainController',
+                //controller: 'mainController',
                 templateUrl: 'Views/Home.html'
             })
             .when('/home', {
-                controller: 'mainController',
                 templateUrl: 'Views/Home.html'
             })
             .when('/products', {
-                //controller: 'productsController',
                 templateUrl: 'Views/Products.html'
             })
             .when('/product/:Id', {
-                //controller: 'productsController',
                 templateUrl: 'Views/Product.html'
             })
             .otherwise({ redirectTo: '/' });
